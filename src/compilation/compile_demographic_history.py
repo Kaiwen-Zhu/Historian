4881078bac33246d33bfb0908acc70106b4ee110
Kaiwen-Zhu <sqzhukaiwen@sjtu.edu.cn>
2023-02-25 17:42:04 +0800

    improve plotting pop size of species

--- src/compilation/compile_demographic_history.py
+++ src/compilation/compile_demographic_history.py
@@ -6,6 +6,23 @@ from pylatex import Section, Subsection, Figure, NoEscape
 from .utils import *
 
 
+def pad_vacant_year(df, species_name):
+    """ Makes the date continuous by setting `num_pop` as 0 and fills `nan` by interpolation.
+    """   
+    prev_year = int(df.iloc[0]['date'][:4])
+    for idx, row in df.iterrows():
+        this_year = int(row['date'][:4])
+        if this_year > prev_year + 1:
+            for vacant_year in range(prev_year+1, this_year):
+                df = pd.concat([df, pd.DataFrame(
+                    {"date": [f"{vacant_year}.01.15"], "species_name": species_name, "num_pop": 0})])
+        prev_year = this_year
+    df.sort_values(by="date", inplace=True)
+    df.reset_index(inplace=True, drop=True) 
+    df['num_pop'].fillna(df['num_pop'].interpolate(), inplace=True)
+    return df
+
+
 def plot_pop_size(data_path, dir_path, lang):
     """ Plots the line chart of population size.
     """    
@@ -13,11 +30,13 @@ def plot_pop_size(data_path, dir_path, lang):
     if lang == 'en':
         file_name = 'Population Size'
         total_label = "Total Size"
+        other_label = "Other {} species"
     else:
         file_name = '人口数量'
         total_label = "总数"
+        others_label = "其它{}个物种"
 
-    plt.figure(figsize=(9, 11))
+    plt.figure(figsize=(9, 13))
 
     # 绘制人口总数
     df = pd.read_csv(path.join(data_path, 'num_pop.csv'), index_col=0, sep=';')
@@ -29,11 +48,41 @@ def plot_pop_size(data_path, dir_path, lang):
             labels = df["date"][::date_step].apply(lambda date: date[:-omitted]), rotation = 30)
 
     # 绘制各物种人口数量
-    df_species = pd.read_csv(path.join(data_path, 'species_pop_size.csv'), index_col=0, sep=';')   
-    species_names = set(df_species['species_name']) 
+    df_species = pd.read_csv(path.join(data_path, 'species_pop_size.csv'), index_col=0, sep=';')  
+
+    # 统计各物种最大数量，仅对前 `MAX_SPECIES_NUM` 个物种单独绘制
+    MAX_SPECIES_NUM = 18
+    max_sizes = []  # 各物种最大数量
+    for species in set(df_species['species_name']):
+        max_size = (species, df_species[df_species['species_name']==species]['num_pop'].max())
+        if max_size[-1] == 0:
+            df_species.drop(index=df_species[df_species['species_name']==species].index, inplace=True)
+        else:
+            max_sizes.append(max_size)
+    max_sizes.sort(key = lambda t: t[-1])
+    others = set(t[0] for t in max_sizes[:-MAX_SPECIES_NUM])  # `others` 中的物种不单独绘制
+
+    species_names = set(df_species['species_name'])
+    other_num = pd.DataFrame(columns=["date", "species_name", "num_pop"])  # 记录其它物种的人口数量之和
     for species in species_names:
-        df_one_species = df_species[df_species['species_name']==species]
-        plt.plot(df_one_species['date'], df_one_species['num_pop'], label=species, alpha=0.8)
+        df_one_species = df_species[df_species['species_name']==species].copy()
+        if species not in others:
+            df_one_species = pad_vacant_year(df_one_species, species)  # 将中间缺失的值补上 0
+            plt.plot(df_one_species['date'], df_one_species['num_pop'], label=species, alpha=0.8)
+        else:
+            df_one_species['num_pop'].fillna(df['num_pop'].interpolate(), inplace=True)
+            for idx, row in df_one_species.iterrows():
+                date, num = row['date'], row['num_pop']
+                if date not in other_num['date'].values:
+                    other_num.loc[len(other_num)] = [date, "others", num]
+                else:
+                    other_num.loc[other_num['date']==date, 'num_pop'] += num
+    # 绘制其它物种人口数量之和
+    if len(other_num) > 0:
+        other_num.sort_values(by='date', inplace=True)
+        other_num.reset_index(inplace=True, drop=True)
+        other_num = pad_vacant_year(other_num, "others")
+        plt.plot(other_num['date'], other_num['num_pop'], label=others_label.format(len(other_num), alpha=0.8))
 
     # plt.xticks(ticks = range(0, len(df_species), date_step), 
             # labels = df_species["date"][::date_step].apply(lambda date: date[:-omitted]), rotation = 30)
