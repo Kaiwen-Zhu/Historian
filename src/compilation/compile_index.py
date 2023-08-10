0c23ac18967f12082fa0d8ce36fe29a592f069a9
Kaiwen-Zhu <sqzhukaiwen@sjtu.edu.cn>
2023-08-10 21:48:45 +0800

    fix bug

--- src/compilation/compile_index.py
+++ src/compilation/compile_index.py
@@ -11,9 +11,9 @@ def compile_index(env: Environment, assets_path: str, output_path: str):
     # 添加 CSS 样式
     css_src_path = path_join(assets_path, 'css', 'general.css')
     css_dst_path = path_join(output_path, 'css', 'general.css')
-    mkdir(path_join(output_path, 'css'))
+    if not path_exists(path_join(output_path, 'css')):
+        mkdir(path_join(output_path, 'css'))
     copyfile(css_src_path, css_dst_path)
-
 
     render_page(env, 'index.html', output_path, '{}史.html'.format(env.globals['name']))
     
