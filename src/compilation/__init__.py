5537caefbd1a281b47da02cf1f16141cdea7a821
Kaiwen-Zhu <sqzhukaiwen@sjtu.edu.cn>
2023-08-15 22:07:17 +0800

    add diplomatic relationships

--- src/compilation/__init__.py
+++ src/compilation/__init__.py
@@ -3,4 +3,4 @@ from .compile_overview import compile_overview
 from .compile_economic_history import compile_economic_history
 from .compile_demographic_history import compile_demographic_history
 from .compile_scientific_history import compile_scientific_history
-# from .compile_diplomatic_history import compile_diplomatic_history
+from .compile_diplomatic_history import compile_diplomatic_history
--- src/compile_history.py
+++ src/compile_history.py
@@ -25,7 +25,7 @@ def main():
     compile_economic_history(env, assets_path, data_path, output_path)
     compile_demographic_history(env, data_path, output_path)
     compile_scientific_history(env, assets_path, data_path, output_path)
-    # compile_diplomatic_history(env, data_path, output_path)
+    compile_diplomatic_history(env, data_path, output_path)
 
     compile_index(env, assets_path, output_path)
 
