0a9b979b1e7daf5d98863ae7576a10465a04aa03
Kaiwen-Zhu <sqzhukaiwen@sjtu.edu.cn>
2023-08-15 17:21:54 +0800

    add pop size

--- src/compilation/compile_demographic_history.py
+++ src/compilation/compile_demographic_history.py
@@ -1,7 +1,6 @@
 from sys import stdout
 from os.path import join as path_join
 import pandas as pd
-from shutil import copyfile
 from jinja2 import Environment
 from .utils import *
 
@@ -32,8 +31,7 @@ def pad_vacant_year(df, species_name, all_dates):
             new_dates.append(date)
         else:
             break
-    df = pd.concat([df, pd.DataFrame(
-                {"date": new_dates, "species_name": species_name, "num_pop": 0})])
+    df = pd.concat([df, pd.DataFrame({"date": new_dates, "species_name": species_name, "num_pop": 0})])
 
     df.sort_values(by="date", inplace=True)
     df.reset_index(inplace=True, drop=True) 
@@ -41,53 +39,42 @@ def pad_vacant_year(df, species_name, all_dates):
     return df
 
 
-def plot_pop_size(data_path, dir_path, lang):
-    """Plots the line chart of population size of species."""    
-
-    if lang == 'en':
-        file_name = 'Population Size'
-        others_label = "Other {} species"
-    else:
-        file_name = '人口数量'
-        others_label = "其它{}个物种"
-
-
+def plot_pop_size(env, data_path, dir_path):
     df_species = pd.read_csv(path_join(data_path, 'species_pop_size.csv'), index_col=0, sep=';')  
-    all_dates = df_species['date'].drop_duplicates(keep='first')
-    all_dates.reset_index(inplace=True, drop=True)
 
+    dates = df_species['date'].drop_duplicates(keep='first')
+    dates.reset_index(inplace=True, drop=True)
+    species_names = df_species['species_name'].drop_duplicates(keep='first').values.tolist()  # 按出现顺序排列
 
-    # 统计各物种最大数量，仅对前 `MAX_SPECIES_NUM` 个物种单独绘制
-    MAX_SPECIES_NUM = 19
-    max_sizes = []  # 各物种最大数量
-    for species in set(df_species['species_name']):
-        max_size = (species, df_species[df_species['species_name']==species]['num_pop'].max())
-        if max_size[-1] == 0:
-            df_species.drop(index=df_species[df_species['species_name']==species].index, inplace=True)
-        else:
-            max_sizes.append(max_size)
-    max_sizes.sort(key = lambda t: t[-1])
-    others = set(t[0] for t in max_sizes[:-MAX_SPECIES_NUM])  # `others` 中的物种不单独绘制
-    num_in_others = len(others)
+    # # 统计各物种最大数量
+    # max_sizes = []  # 各物种最大数量
+    # for species in set(df_species['species_name']):
+    #     max_size = (species, df_species[df_species['species_name']==species]['num_pop'].max())
+    #     if max_size[-1] == 0:
+    #         df_species.drop(index=df_species[df_species['species_name']==species].index, inplace=True)
+    #     else:
+    #         max_sizes.append(max_size)
+    # max_sizes.sort(key = lambda t: t[-1])
 
-    # 按出现顺序统计物种名称
-    species_names = df_species['species_name'].drop_duplicates(keep='first').values.tolist()
+    pop_size_config = {
+        'title': "人口规模", 'x': dates.apply(lambda date: date[:-3]).tolist(), 'data': []
+    }
     
-    pop_sizes = []
-    other_num = pd.DataFrame(columns=["date", "species_name", "num_pop"])  # 记录其它物种的人口数量之和
     for species in species_names:
         df_one_species = df_species[df_species['species_name']==species].copy()
-        df_one_species = pad_vacant_year(df_one_species, species, all_dates)  # 将中间缺失的值补上 0
-        if species not in others:
-            # 可能有多个物种同名，需要合并
-            nums = []
-            for date in all_dates:
-                nums.append(df_one_species[df_one_species['date']==date]['num_pop'].sum())
-            assert len(all_dates) == len(nums)
-            pop_sizes.append(nums)
+        df_one_species = pad_vacant_year(df_one_species, species, dates)  # 将缺失的值补上 0
+        
+        # 可能有多个物种同名，需要合并
+        nums = []
+        for date in dates:
+            nums.append(df_one_species[df_one_species['date']==date]['num_pop'].sum())
+        assert len(dates) == len(nums)
+        pop_size_config['data'].append({
+            'name': species, 'data': nums
+        })
+    
+    render_page(env, 'pop_size.html', dir_path, '人口规模.html', config=pop_size_config)
                     
-    # plt.stackplot(all_dates, pop_sizes, colors=colors)
-
 
 def plot_unity(env, data_path, dir_path):
     df = pd.read_csv(path_join(data_path, 'resources.csv'), index_col=0, sep=';')
@@ -119,7 +106,7 @@ def compile_demographic_history(env: Environment, data_path: str, output_path: s
 
     dir_path = prepare_output(output_path, "人口史")
 
-    # plot_pop_size(env, data_path, dir_path)
+    plot_pop_size(env, data_path, dir_path)
     plot_unity(env, data_path, dir_path)
 
     print("完成")
